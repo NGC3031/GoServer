8c1df83b76ab88b966501dce8e322d6a76473d3e
ScipioTheKangaroo <43354084+ScipioTheKangaroo@users.noreply.github.com>
2018-10-18 08:26:24 +1100

    Add files via upload

---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// The new router function creates the router and returns it.
+// Having it in a seperate function makes for easier flexibility and testing
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the newRouter function.
+	r := newRouter()
+	err := http.ListenAndServe(":8070", r)
+	if err != nil {
+		log.Fatal("Web server (HTTP): ", err)
+	} else {
+		fmt.Println("Server started")
+	}
+}
+
+//Main Handler functions
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
